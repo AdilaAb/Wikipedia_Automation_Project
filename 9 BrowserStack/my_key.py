5627cb22af304c5ca39a31ee0c1061baefe52888
Adila <adelya0927@gmail.com>
2023-11-13 18:11:38 -0500

    Updated files

--- 9 BrowserStack/my_key.py
+++
@@ -1,8 +0,0 @@
-# replace data in the quotes for yourown data
-
-
-BROWSERSTACK_USERNAME = "xxxxxxx"
-BROWSERSTACK_ACCESS_KEY = "xxxxxxxxxxx"
-
-# If someone can’t install dotenv try the following in terminal
-#pip3 install python-dotenv
