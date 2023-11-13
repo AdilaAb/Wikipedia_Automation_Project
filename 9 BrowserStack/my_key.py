4c123ce7278935b83bf86003d86d008a91448143
Adila Abdullayeva <141700482+AdilaAb@users.noreply.github.com>
2023-11-13 06:20:52 -0500

    Update my_key.py

--- 9 BrowserStack/my_key.py
+++ 9 BrowserStack/my_key.py
@@ -1,8 +1,8 @@
 # replace data in the quotes for yourown data
 
-key = 'https://adilaa_1ZfqsY:9zsdfGxnYJgiVAek97aD@hub-cloud.browserstack.com/wd/hub'
-BROWSERSTACK_USERNAME = "adilaa_1ZfqsY"
-BROWSERSTACK_ACCESS_KEY = "9zsdfGxnYJgiVAek97aD"
+
+BROWSERSTACK_USERNAME = "xxxxxxx"
+BROWSERSTACK_ACCESS_KEY = "xxxxxxxxxxx"
 
 # If someone can’t install dotenv try the following in terminal
 #pip3 install python-dotenv
